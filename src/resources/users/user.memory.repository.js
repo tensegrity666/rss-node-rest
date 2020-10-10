dcfa2b124f27ece9ece80a7e35b1c309f8d3c4fe
Roman Yakovlev <tensegrity666@icloud.com>
2020-10-10 23:06:47 +0300

    feat: implement users-rest

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,25 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+const {
+  getAllUsers,
+  getUser,
+  createUser,
+  deleteUser,
+  updateUser
+} = require('../../common/db-users');
+
+const getAll = async () => await getAllUsers();
+
+const get = async id => {
+  const user = await getUser(id);
+
+  if (user === undefined) throw new Error(`User with id:${id} not found!`);
+
+  return user;
 };
 
-module.exports = { getAll };
+const create = async user => await createUser(user);
+
+const del = async id => await deleteUser(id);
+
+const update = async (id, value) => await updateUser(id, value);
+
+module.exports = { getAll, get, create, del, update };
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,9 @@
 const usersRepo = require('./user.memory.repository');
 
 const getAll = () => usersRepo.getAll();
+const get = id => usersRepo.get(id);
+const create = user => usersRepo.create(user);
+const del = user => usersRepo.del(user);
+const update = (id, value) => usersRepo.update(id, value);
 
-module.exports = { getAll };
+module.exports = { getAll, get, create, del, update };
