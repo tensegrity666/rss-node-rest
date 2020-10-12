6ec399df6577fe01b0a4b2590bf374fc9e0640f1
Roman Yakovlev <tensegrity666@icloud.com>
2020-10-12 20:21:52 +0300

    feat: del task

--- src/resources/tasks/task.router.js
+++ src/resources/tasks/task.router.js
@@ -1,5 +1,4 @@
 const router = require('express').Router({ mergeParams: true });
-const boardsService = require('../boards/board.service');
 const tasksService = require('./task.service');
 
 router.route('/').get(async (req, res) => {
@@ -22,6 +21,13 @@ router.route('/:taskId').get(async (req, res) => {
   }
 });
 
+router.route('/:taskId').delete(async (req, res) => {
+  const tasks = await tasksService.del(req.params.boardId, req.params.taskId);
+
+  res.json(tasks);
+});
+
+// ! ПЕРЕДЕЛАТЬ
 router.route('/:taskId').put(async (req, res) => {
   const taskFragment = {
     title: req.body.title,
@@ -32,17 +38,12 @@ router.route('/:taskId').put(async (req, res) => {
     columnId: req.body.columnId
   };
 
+  // eslint-disable-next-line no-undef
   const board = await boardsService.get(req.params.boardId);
   const { tasks } = board;
   const task = await tasks.filter(el => el.id === req.params.taskId);
 
   res.json(Object.assign(...task, taskFragment));
-});
-
-router.route('/:taskId').delete(async (req, res) => {
-  const board = await boardsService.get(req.params.boardId);
-  const { tasks } = board;
-  res.json(tasks.filter(el => el.id !== req.params.taskId));
 });
 
 // router.route('/:id').put(async (req, res) => {
