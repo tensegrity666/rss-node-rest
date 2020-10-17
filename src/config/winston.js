df885dc93452f06c75db64e724ffafa2c7d89f01
Roman Yakovlev <tensegrity666@icloud.com>
2020-10-18 01:38:27 +0300

    feat: logging

--- src/config/winston.js
+++ src/config/winston.js
@@ -1,18 +1,45 @@
+/* eslint-disable no-unused-expressions */
 /* eslint-disable no-unused-vars */
 
 const winston = require('winston');
+const { createLogger, format, transports } = require('winston');
+const {
+  splat,
+  combine,
+  timestamp,
+  printf,
+  simple,
+  json,
+  prettyPrint,
+  colorize
+} = format;
 const { NODE_ENV } = require('./config');
 
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
+const logger = createLogger({
   transports: [
-    new winston.transports.File({
-      filename: './logs/error.log',
-      level: 'error'
+    new transports.File({
+      filename: './logs/combined.log',
+      level: 'info',
+      format: combine(
+        timestamp(),
+        printf(info => `${info.timestamp} [${info.level}]: ${info.message}`)
+      )
     }),
-    new winston.transports.File({ filename: './logs/combined.log' })
+    new transports.File({
+      filename: './logs/error.log',
+      level: 'error',
+      format: combine(
+        timestamp(),
+        printf(info => `${info.timestamp} [${info.level}]: ${info.message}`)
+      )
+    }),
+    new transports.Http({
+      level: 'warn',
+      format: combine(
+        timestamp(),
+        printf(info => `${info.timestamp} [${info.level}]: ${info.message}`)
+      )
+    })
   ]
 });
 
@@ -20,14 +47,21 @@ if (NODE_ENV !== 'production') {
   logger.add(
     new winston.transports.Console({
       level: 'debug',
-      format: winston.format.simple()
+      format: combine(colorize(), prettyPrint(), simple())
     })
   );
 }
 
+logger.exitOnError = false;
+
 logger.stream = {
   write(message, encoding) {
-    logger.info(message);
+    if (message !== undefined) {
+      message.substring(0, 3) === '500'
+        ? logger.error('Internal Server Error')
+        : logger.info(message);
+    }
+    logger.info('Error');
   }
 };
 
