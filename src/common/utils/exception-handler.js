80f4dd60f4353be3bcef65a1ecc0a204d35f44f2
Roman Yakovlev <tensegrity666@icloud.com>
2020-10-23 23:54:50 +0300

    fix: add timeout and exit

--- src/common/utils/exception-handler.js
+++ src/common/utils/exception-handler.js
@@ -1,18 +1,16 @@
+/* eslint-disable no-process-exit */
+
 const { logger, errors } = require('../../config/winston');
-const chalk = require('chalk');
 
 const exceptionHandler = (err, origin) => {
-  const message = `Caught exception: ${err.stack} - Exception origin: ${origin}`;
+  const message = `Caught exception: ${err.message} - Exception origin: ${origin}`;
 
   errors.error(message);
   logger.error(message);
 
-  console.log(
-    chalk.bgRed.white.bold(
-      '\nApp must be aborted because:\n' +
-        'https://nodejs.org/api/process.html#process_warning_using_uncaughtexception_correctly \n'
-    )
-  );
+  setTimeout(() => {
+    process.exit(1);
+  }, 100);
 };
 
 module.exports = exceptionHandler;
